2117b074513f9964de3a7f250d70e5480dd0588d
Saul De la fuente <saul.delafuenteb@gmail.com>
2024-04-24 16:40:30 -0600

    Primer commit

---
+++ noduloConsultaResultados/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState,useEffect } from 'react'
+
+import './App.css'
+
+function MenuResultados(){
+  
+
+}
+
+
+function App() {
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
